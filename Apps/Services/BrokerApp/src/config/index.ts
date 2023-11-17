8e649049c71d1ed61239b83d503c439f4938c87d
David Leclerc <d.leclerc.pro@gmail.com>
2023-11-17 08:56:51 +0100

    Configure URI components of services within broker.

--- Apps/Services/BrokerApp/src/config/index.ts
+++ Apps/Services/BrokerApp/src/config/index.ts
@@ -6,19 +6,21 @@ export const ENV = loadEnvironment();
 export const PROTOCOL = process.env.PROTOCOL;
 export const HOST = process.env.HOST;
 export const PORT = process.env.PORT;
-
 export const ROOT = `${PROTOCOL}://${HOST}:${PORT}`;
 
+const { ORDER_APP_PROTOCOL, ORDER_APP_HOST, ORDER_APP_PORT } = process.env;
 export const ORDER_SERVICE: Service = {
-    uri: 'http://localhost:3001',
+    uri: `${ORDER_APP_PROTOCOL}://${ORDER_APP_HOST}:${ORDER_APP_PORT}`,
 };
 
+const { PAYMENT_APP_PROTOCOL, PAYMENT_APP_HOST, PAYMENT_APP_PORT } = process.env;
 export const PAYMENT_SERVICE: Service = {
-    uri: 'http://localhost:3002',
+    uri: `${PAYMENT_APP_PROTOCOL}://${PAYMENT_APP_HOST}:${PAYMENT_APP_PORT}`,
 };
 
+const { DELIVERY_APP_PROTOCOL, DELIVERY_APP_HOST, DELIVERY_APP_PORT } = process.env;
 export const DELIVERY_SERVICE: Service = {
-    uri: 'http://localhost:3003',
+    uri: `${DELIVERY_APP_PROTOCOL}://${DELIVERY_APP_HOST}:${DELIVERY_APP_PORT}`,
 };
 
 export const SERVICES = [ORDER_SERVICE, PAYMENT_SERVICE, DELIVERY_SERVICE];
