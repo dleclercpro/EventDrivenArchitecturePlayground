7b96228dbef051c13f014c7de9bb0b487419a797
David Leclerc <d.leclerc.pro@gmail.com>
2023-11-20 22:45:38 +0100

    Add completion of subscriptions to health check of services.

--- Apps/PaymentApp/src/controllers/HealthController.ts
+++ Apps/PaymentApp/src/controllers/HealthController.ts
@@ -1,13 +1,16 @@
 import { RequestHandler } from 'express';
 import { HttpStatusCode } from '../../../CommonApp/src/types/HTTPTypes';
 import logger from '../logger';
+import { subscriber } from '..';
 
 const HealthController: RequestHandler = async (req, res) => {
     try {
         logger.debug(`Health check: OK`);
 
+        const status = subscriber.hasSubscribed() ? HttpStatusCode.OK : HttpStatusCode.SERVICE_UNAVAILABLE;
+
         // Success
-        return res.sendStatus(HttpStatusCode.OK);
+        return res.sendStatus(status);
 
     } catch (err: any) {
 
