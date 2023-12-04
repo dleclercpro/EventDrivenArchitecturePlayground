2712b410d486fcca92ef6988596238acf78c1e54
David Leclerc <d.leclerc.pro@gmail.com>
2023-12-04 18:23:01 +0100

    Docker image tags on master are called "latest".

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ cd "$dir/Apps"
 # Define constant image details
 user="dleclercpro"
 app="eda-playground"
-release="v1.2.0"
+release="latest"
 
 # Build app images
 docker build -t $user/$app-broker:$release -f Broker.Dockerfile .
