1822912f55b2656b3033d26514659cccb141af91
David Leclerc <d.leclerc.pro@gmail.com>
2023-12-22 16:54:44 -0500

    Missing ready controller routes.

--- Apps/Delivery/src/routes/index.ts
+++ Apps/Delivery/src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import HealthController from '../controllers/HealthController';
+import ReadyController from '../controllers/ReadyController';
 import NotifyController from '../controllers/NotifyController';
 
 
@@ -9,7 +10,11 @@ const router = Router();
 
 
 // ROUTES
+// Probes
 router.get('/health', HealthController);
+router.get('/ready', ReadyController);
+
+// API
 router.post(`/notify`, NotifyController);
 
 
