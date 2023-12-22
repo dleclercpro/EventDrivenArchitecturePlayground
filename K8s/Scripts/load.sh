05aca640f52a60d733e9371310b55f7a76fd76be
David Leclerc <d.leclerc.pro@gmail.com>
2023-12-22 10:05:42 -0500

    Renamed script.

--- K8s/Scripts/load.sh
+++ K8s/Scripts/load.sh
@@ -3,7 +3,7 @@ user="dleclercpro"
 app="eda-playground"
 release="k8s"
 
-# Upload local images to Minikube registry
+# Upload local images to Minikube image registry
 minikube image load $user/$app-broker:$release
 minikube image load $user/$app-order:$release
 minikube image load $user/$app-payment:$release
