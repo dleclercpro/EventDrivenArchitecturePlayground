1a07ce48f947586940672805ce2cf06a250638d1
David Leclerc <d.leclerc.pro@gmail.com>
2023-12-21 22:38:11 -0500

    Split of Docker image build and push to registry. Removal of simple docker command script.

--- push.sh
+++ push.sh
@@ -9,10 +9,16 @@ cd "$dir/Apps"
 # Define constant image details
 user="dleclercpro"
 app="eda-playground"
-release="latest"
+release="k8s"
 
 # Build app images
 docker build -t $user/$app-broker:$release -f Broker.Dockerfile .
 docker build -t $user/$app-order:$release -f Order.Dockerfile .
 docker build -t $user/$app-payment:$release -f Payment.Dockerfile .
 docker build -t $user/$app-delivery:$release -f Delivery.Dockerfile .
+
+# Push app images to Dockerhub
+docker push $user/$app-broker:$release
+docker push $user/$app-order:$release
+docker push $user/$app-payment:$release
+docker push $user/$app-delivery:$release
