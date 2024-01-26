876dc7694da16355cd43b1f4d919d1763bfae100
David Leclerc <d.leclerc.pro@gmail.com>
2024-01-26 04:44:24 +0100

    Release tag.

--- K8s/Scripts/load.sh
+++ K8s/Scripts/load.sh
@@ -1,7 +1,7 @@
 # Define constant image details
 user="dleclercpro"
 app="eda-playground"
-release="latest"
+release="pretty-frontend"
 
 # Upload local images to Minikube image registry
 minikube image load $user/$app-broker:$release
--- Scripts/build.sh
+++ Scripts/build.sh
@@ -9,7 +9,7 @@ cd "$dir/../Apps"
 # Define constant image details
 user="dleclercpro"
 app="eda-playground"
-release="latest"
+release="pretty-frontend"
 
 # Build app images
 docker buildx build --platform linux/amd64,linux/arm64 -t $user/$app-broker:$release -f Broker.Dockerfile . --push
